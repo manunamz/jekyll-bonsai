f5ea8062219cafc0fb6be7ab3a81ab1b2c752a3c
manunamz <manunamz@hey.com>
2021-04-12 12:26:44 -0400

    add: center of gravity added so nodes stay visible in sidebar; removed unused code; added some comments.

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -1,128 +1,50 @@
-// graph.js
+// import { dragstarted, dragged, dragended } from "./drag.js";
+
 export default function drawGraph () {
   // d3.json has been async'd: https://stackoverflow.com/questions/49768165/code-within-d3-json-callback-is-not-executed 
   d3.json("/assets/notes_graph.json")
     .then(function(data) {
-        // console.log('d3 is building a graph');
-        // console.log(graph);
+        // console.log('d3 is building a tree');
+        // console.log(data);
         const svgWrapper = document.getElementById('svg-nav');
-        var svg = d3.select(svgWrapper),
-            width = +svg.node().getBoundingClientRect().width,
-            height = +svg.node().getBoundingClientRect().height;
+        var width = +svgWrapper.getBoundingClientRect().width / 2;
+        var height = +svgWrapper.getBoundingClientRect().height / 2;
+        var svg = d3.select(svgWrapper)
+            .attr("viewBox", [-width / 2, -height / 2, width, height]);
         
-        var graph = data;
-        // svg objects
-        var link, node;    
-        // values for all forces
-        var forceProperties = {
-            center: {
-                x: 0.5,
-                y: 0.5
-            },
-            charge: {
-                enabled: true,
-                strength: -30,
-                distanceMin: 1,
-                distanceMax: 2000
-            },
-            collide: {
-                enabled: true,
-                strength: .7,
-                iterations: 1,
-                radius: 5
-            },
-            forceX: {
-                enabled: false,
-                strength: .1,
-                x: .5
-            },
-            forceY: {
-                enabled: false,
-                strength: .1,
-                y: .5
-            },
-            link: {
-                enabled: true,
-                distance: 30,
-                iterations: 1
-            }
-        }
-        // force simulator
-        var simulation = d3.forceSimulation();
-        initializeDisplay();
-        initializeSimulation();
+        const radius = 3.5;
 
-        //////////// FORCE simulation //////////// 
-
-        // set up the simulation and event to update locations after each tick
-        function initializeSimulation() {
-            simulation.nodes(graph.nodes);
-            initializeForces();
-            simulation.on("tick", ticked);
-        }
-
-        // add forces to the simulation
-        function initializeForces() {
-            // add forces and associate each with a name
-            simulation
-                .force("link", d3.forceLink())
-                .force("charge", d3.forceManyBody())
-                .force("collide", d3.forceCollide())
-                .force("center", d3.forceCenter())
-                .force("forceX", d3.forceX())
-                .force("forceY", d3.forceY());
-            // apply properties to each of the forces
-            updateForces();
-        }
-
-        // apply new force properties
-        function updateForces() {
-            // get each force by name and update the properties
-            simulation.force("center")
-                .x(width * forceProperties.center.x)
-                .y(height * forceProperties.center.y);
-            simulation.force("charge")
-                .strength(forceProperties.charge.strength * forceProperties.charge.enabled)
-                .distanceMin(forceProperties.charge.distanceMin)
-                .distanceMax(forceProperties.charge.distanceMax);
-            simulation.force("collide")
-                .strength(forceProperties.collide.strength * forceProperties.collide.enabled)
-                .radius(forceProperties.collide.radius)
-                .iterations(forceProperties.collide.iterations);
-            simulation.force("forceX")
-                .strength(forceProperties.forceX.strength * forceProperties.forceX.enabled)
-                .x(width * forceProperties.forceX.x);
-            simulation.force("forceY")
-                .strength(forceProperties.forceY.strength * forceProperties.forceY.enabled)
-                .y(height * forceProperties.forceY.y);
-            simulation.force("link")
+        const simulation = d3.forceSimulation()
+            .nodes(data.nodes)
+            .force("link", d3.forceLink()
                 .id(function(d) {return d.id;})
-                .distance(forceProperties.link.distance)
-                .iterations(forceProperties.link.iterations)
-                .links(forceProperties.link.enabled ? graph.links : []);
+                .distance(30)
+                .iterations(1)
+                .links(data.links))
+            .force("charge", d3.forceManyBody())
+            .force("collide", d3.forceCollide())
+            .force("center", d3.forceCenter())
+            // see: https://stackoverflow.com/questions/9573178/d3-force-directed-layout-with-bounding-box?answertab=votes#tab-top
+            // 'center of gravity'
+            .force("forceX", d3.forceX()
+                .strength(.5)
+                .x(.75))
+            .force("forceY", d3.forceY()
+                .strength(.1)
+                .y(.9));
 
-            // updates ignored until this is run
-            // restarts the simulation (important if simulation has already slowed down)
-            simulation.alpha(1).restart();
-        }
-
-        //////////// DISPLAY ////////////
-
-        // generate the svg objects and force simulation
-        function initializeDisplay() {
-        // set the data and properties of link lines
-        link = svg.append("g")
-                .attr("class", "links")
+        const link = svg.append("g")
+            .attr("class", "links")
             .selectAll("line")
-            .data(graph.links)
+            .data(data.links)
             .enter().append("line");
 
-        // set the data and properties of node circles
-        node = svg.append("g")
+        const node = svg.append("g")
                 .attr("class", "nodes")
-            .selectAll("circle")
-            .data(graph.nodes)
-            .enter().append("circle")
+                .selectAll("circle")
+                .data(data.nodes)
+                .enter().append("circle")
+                .attr("r", radius)
                 .call(d3.drag()
                     .on("start", dragstarted)
                     .on("drag", dragged)
@@ -131,24 +53,8 @@ export default function drawGraph () {
         // node tooltip
         node.append("title")
             .text(function(d) { return d.label; });
-        // visualize the graph
-        updateDisplay();
-        }
-
-        // update the display based on the forces (but not positions)
-        function updateDisplay() {
-            node
-                .attr("r", forceProperties.collide.radius)
-                .attr("stroke", forceProperties.charge.strength > 0 ? "blue" : "#31AF31")
-                .attr("stroke-width", forceProperties.charge.enabled==false ? 0 : Math.abs(forceProperties.charge.strength)/15);
-
-            link
-                .attr("stroke-width", forceProperties.link.enabled ? 1 : .5)
-                .attr("opacity", forceProperties.link.enabled ? 1 : 0);
-        }
-
-        // update the display positions after each simulation tick
-        function ticked() {
+        
+        simulation.on("tick", () => {
             link
                 .attr("x1", function(d) { return d.source.x; })
                 .attr("y1", function(d) { return d.source.y; })
@@ -158,13 +64,7 @@ export default function drawGraph () {
             node
                 .attr("cx", function(d) { return d.x; })
                 .attr("cy", function(d) { return d.y; });
-            d3.select('#alpha_value').style('flex-basis', (simulation.alpha()*100) + '%');
-        }
-
-
-
-        //////////// UI EVENTS ////////////
-
+        });
 
         function dragstarted(event) {
             if (!event.active) simulation.alphaTarget(0.3).restart();
@@ -181,19 +81,6 @@ export default function drawGraph () {
             if (!event.active) simulation.alphaTarget(0);
             event.subject.fx = null;
             event.subject.fy = null;
-        }
-
-        // update size-related forces
-        d3.select(window).on("resize", function(){
-            width = +svg.node().getBoundingClientRect().width;
-            height = +svg.node().getBoundingClientRect().height;
-            updateForces();
-        });
-
-        // convenience function to update everything (run after UI input)
-        function updateAll() {
-            updateForces();
-            updateDisplay();
         }
     })
     .catch(function(error) {
--- assets/js/tree.js
+++ assets/js/tree.js
@@ -1,76 +1,85 @@
-// tree.js
+// import { dragstarted, dragged, dragended } from "./drag.js";
+
 export default function drawTree () { 
   // d3.json has been async'd: https://stackoverflow.com/questions/49768165/code-within-d3-json-callback-is-not-executed 
     d3.json("/assets/notes_tree.json")
     .then(function(data) {
-
         // console.log('d3 is building a graph');
-        // console.log(graph);
-        
-        // var data = {"id":"ba626641-0703-46aa-807d-61c497a0374e","namespace":"root","name":"Root","children":[{"id":"81c12a31-fcfe-423a-a3d7-b45dd476e12d","namespace":"root.data-structure","name":"Data Structure","children":[{"id":"93d4ce86-9aa0-47bb-8555-48a6f2f54b14","namespace":"root.data-structure.graph","name":"Graph","children":[]},{"id":"e0f8dea6-1762-4623-b46e-f402d1a2fa0c","namespace":"root.data-structure.tree","name":"Tree","children":[]}]},{"id":"3921276c-f9cc-4748-8272-04b268207a32","namespace":"root.garden","name":"Garden","children":[{"id":"","namespace":"root.garden.lifecycle","name":"","children":[{"id":"9c98f004-cae7-453a-99f7-9510142b7600","namespace":"root.garden.lifecycle.bamboo","name":"Bamboo","children":[]},{"id":"722f3e95-455c-42a5-9030-09e31248a2a3","namespace":"root.garden.lifecycle.bud","name":"Bud","children":[]},{"id":"d70543bd-3d7f-440c-9cd7-8aad889a6928","namespace":"root.garden.lifecycle.sprout","name":"Sprout","children":[]}]}]}]};
-
+        // console.log(data);
         const svgWrapper = document.getElementById('svg-nav');
+        
+        var width = +svgWrapper.getBoundingClientRect().width / 2;
+        var height = +svgWrapper.getBoundingClientRect().height / 2;
+        
+        var svg = d3.select(svgWrapper)
+            .attr("viewBox", [-width / 2, -height / 2, width, height]);
+  
+        const radius = 3.5;
 
         const root = d3.hierarchy(data);
         const links = root.links();
+        // flatten(root);
         const nodes = root.descendants();
 
-        const treeSim = d3.forceSimulation(nodes)
+        const simulation = d3.forceSimulation(nodes)
             .force("link", d3.forceLink(links).id(d => d.id).distance(0).strength(1))
             .force("charge", d3.forceManyBody().strength(-50))
-            .force("x", d3.forceX())
-            .force("y", d3.forceY());
-
-        // const svg = d3.create("svg")
-        //     .attr("viewBox", [-width / 2, -height / 2, width, height]);
-
-        var svg = d3.select(svgWrapper),
-            width = +svg.node().getBoundingClientRect().width,
-            height = +svg.node().getBoundingClientRect().height;
-
-            const link = svg.append("g")
-                .attr("stroke", "#999")
-                .attr("stroke-opacity", 0.6)
+            .force("collide", d3.forceCollide())
+            .force("center", d3.forceCenter())
+            // see: https://stackoverflow.com/questions/9573178/d3-force-directed-layout-with-bounding-box?answertab=votes#tab-top
+            // 'center of gravity'
+            .force("forceX", d3.forceX())
+            .force("forceY", d3.forceY());
+  
+        const link = svg.append("g")
+            .attr("class", "links")
             .selectAll("line")
             .data(links)
             .join("line");
 
         const node = svg.append("g")
-            .attr("fill", "#fff")
-            .attr("stroke", "#000")
-            .attr("stroke-width", 1.5)
-        .selectAll("circle")
-        .data(nodes)
-        .join("circle")
-            .attr("fill", d => d.children ? null : "#000")
-            .attr("stroke", d => d.children ? null : "#fff")
-            .attr("r", 3.5)
+            .attr("class", "nodes")
+            .selectAll("circle")
+            .data(nodes)
+            .join("circle")
+            .attr("r", radius)
             .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
-
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended));
+        
+        // node tooltip
         node.append("title")
-            .text(d => d.data.name);
+            .text(function(d) { return d.data.name });
 
-        treeSim.on("tick", () => {
-        link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
+        simulation.on("tick", () => {
+            // from: https://mbostock.github.io/d3/talk/20110921/parent-foci.html
+            // preserve hierarchical shape via link positioning
+            var kx = .2 * simulation.alpha();
+            var ky = 1.2 * simulation.alpha();
+            links.forEach(function(d, i) {
+              d.target.x += (d.source.x - d.target.x) * kx;
+              d.target.y += (d.source.y + 40 - d.target.y) * ky;
+            });
 
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
+            link
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
+
+            node
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+                
         });
 
-        // invalidation.then(() => treeSim.stop());
+        // invalidation.then(() => simulation.stop());
 
         // return svg.node();
 
         function dragstarted(event, d) {
-            if (!event.active) treeSim.alphaTarget(0.3).restart();
+            if (!event.active) simulation.alphaTarget(0.3).restart();
             d.fx = d.x;
             d.fy = d.y;
         }
@@ -81,7 +90,7 @@ export default function drawTree () {
         }
 
         function dragended(event, d) {
-            if (!event.active) treeSim.alphaTarget(0);
+            if (!event.active) simulation.alphaTarget(0);
             d.fx = null;
             d.fy = null;
         }
