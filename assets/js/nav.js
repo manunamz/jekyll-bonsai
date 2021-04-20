5544bf9dbebcbcbe7581f1c8e924fcad02dc6744
shorty25h0r7 <short2thingz@gmail.com>
2021-04-20 11:59:14 -0400

    ref: cleanup nav.js and its related html in default.html.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,50 +3,70 @@
 import drawTree from './tree.js';
 import drawGraph from './graph.js';
 
+ //
+// go
+ //
+
 // from: https://stackoverflow.com/questions/9899372/pure-javascript-equivalent-of-jquerys-ready-how-to-call-a-function-when-t
 (function() {
     // your page initialization code here
     // the DOM will be available here
+    initListeners();
+    initDefaults();
+ })();
 
+ //
+// init
+ //
+function initDefaults () {
+  if ('{{ site.theme_colors }}' === "dark") { 
+    document.getElementById('theme-colors-checkbox').checked = true; 
+  } else {
+    document.getElementById('theme-colors-checkbox').checked = false; 
+  }
+  if ('{{ site.graph_type }}' === "tree") { 
+    document.getElementById('graph-type-checkbox').checked = true;
+  } else {
+    document.getElementById('graph-type-checkbox').checked = false;
+  }
+  updateColors();
+  updateGraphTypeEmoji();
+  drawD3Nav();
+}
+
+function initListeners () {
     // don't use 'onclick' in html: https://stackoverflow.com/questions/17378199/uncaught-referenceerror-function-is-not-defined-with-onclick
     // prefer explicit registration of event listeners: https://stackoverflow.com/questions/12627443/jquery-click-vs-onclick/12627478#12627478
     // attach event listener to graphTypeCheckbox
-    const colorsCheckBox = document.getElementById('colors-checkbox');
-    colorsCheckBox.addEventListener('click', function(event) {
-      drawD3Nav();
-      changeColors();
-    }, false);
+    document.getElementById('theme-colors-checkbox')
+      .addEventListener('click', function(event) {
+        drawD3Nav();
+        updateColors();
+      }, false);
     
-    const graphTypeCheckBox = document.getElementById('graph-type-checkbox');
-    graphTypeCheckBox.addEventListener('click', function(event) {
-      drawD3Nav();
-      changeGraphType();
-    }, false);
+    document.getElementById('graph-type-checkbox')
+      .addEventListener('click', function(event) {
+        drawD3Nav();
+        updateGraphTypeEmoji();
+      }, false);
 
-    const wikiNavCheckBox = document.getElementById('wiki-link-nav-checkbox');
-    wikiNavCheckBox.addEventListener('click', function(event) {
-      expandGraphNav();
-      drawD3Nav();
-    }, false);
+    document.getElementById('wiki-link-nav-checkbox')
+      .addEventListener('click', function(event) {
+        expandGraphNav();
+        drawD3Nav();
+      }, false);
+}
 
-    // jtd.getTheme = function() {
-    //   const cssFileHref = document.querySelector('[rel="stylesheet"]').getAttribute('href');
-    //   return cssFileHref.substring(cssFileHref.lastIndexOf('-') + 1, cssFileHref.length - 4);
-    // }
-    
-    // todo: set to default theme value. 
-    // todo: set to config default value.
-    // todo: once values are set from config, make sure default.html does the same.
-    drawTree();
- })();
+ //
+// helpers
+ //
 
-function changeColors () {
+function updateColors () {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  const colorsCheckBox = document.getElementById('colors-checkbox');
   const colorsEmojiSpan = document.getElementById('colors-emoji-span');
 
   var theme_colors = "dark";
-  if (colorsCheckBox.checked) {
+  if (document.getElementById('theme-colors-checkbox').checked) {
     colorsEmojiSpan.innerHTML = "☀️";
     theme_colors = "dark";
   } else {
@@ -57,10 +77,9 @@ function changeColors () {
   cssFile.setAttribute('href', '{{ "assets/css/styles-" | absolute_url }}' + theme_colors + yesThisReallyIsSupposedToBeCSSNotSCSS);
 }
 
-function changeGraphType () {
-  const graphTypeCheckBox = document.getElementById('graph-type-checkbox');
+function updateGraphTypeEmoji () {
   const graphTypeEmojiSpan = document.getElementById('graph-type-emoji-span');
-  if (graphTypeCheckBox.checked) {
+  if (document.getElementById('graph-type-checkbox').checked) {
     graphTypeEmojiSpan.innerHTML = "🕸";
   } else {
     graphTypeEmojiSpan.innerHTML = "🌳";
