dac0cded7d79ea3a8b5907defd9c183cc336c726
manunamz <manunamz@hey.com>
2021-05-06 15:03:11 -0400

    add: add unit tests for wikilink plugin.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,108 +1,44 @@
-# 
-# ATTEMPT 1
-# 
-
-# # spec_helper.rb
-# # from: https://ayastreb.me/writing-a-jekyll-plugin/
-# $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-# require "jekyll"
-
-# Jekyll.logger.log_level = :error
-
-# RSpec.configure do |config|
-#   config.run_all_when_everything_filtered = true
-#   config.filter_run :focus
-#   config.order = "random"
-
-#   SOURCE_DIR = File.expand_path("md_notes", __FILE__)
-#   DEST_DIR   = File.expand_path("dest", __FILE__)
-
-#   def source_dir(*files)
-#     File.join(SOURCE_DIR, *files)
-#   end
-
-#   def dest_dir(*files)
-#     File.join(DEST_DIR, *files)
-#   end
-
-#   CONFIG_DEFAULTS = {
-#     "source"      => source_dir,
-#     "destination" => dest_dir,
-#   }.freeze
-
-#   def make_page(options = {})
-#     page      = Jekyll::Page.new(site, CONFIG_DEFAULTS["source"], "", "page.md")
-#     page.data = options
-#     page
-#   end
-
-#   def make_site(options = {})
-#     site_config = Jekyll.configuration(CONFIG_DEFAULTS.merge(options))
-#     Jekyll::Site.new(site_config)
-#   end
-
-#   def make_context(registers = {}, environments = {})
-#     Liquid::Context.new(environments, {}, 
-#       { :site => site, :page => page }.merge(registers))
-#   end
-# end
-
-# 
-# ATTEMPT 2
-# 
-
-# require 'bundler/setup'
-# require 'jekyll'
-
-# RSpec.configure do |config|
-#   # Enable flags like --only-failures and --next-failure
-#   # config.example_status_persistence_file_path = ".rspec_status"
-
-#   # Disable RSpec exposing methods globally on `Module` and `main`
-#   config.disable_monkey_patching!
-
-#   config.expect_with :rspec do |c|
-#     c.syntax = :expect
-#   end
-# end
-
-# 
-# ATTEMPT 3
-# 
 # frozen_string_literal: true
 require 'jekyll'
 
 Jekyll.logger.log_level = :error
 
+# comments from: https://github.com/jekyll/jekyll-mentions/blob/master/spec/spec_helper.rb
 RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-  config.order = "random"
-
-  SOURCE_DIR = File.expand_path("fixtures", __dir__)
+  FIXTURES_DIR = File.expand_path("fixtures", __dir__)
   DEST_DIR   = File.expand_path("dest",     __dir__)
 
-  def source_dir(*files)
-    File.join(SOURCE_DIR, *files)
+  def fixtures_dir(*files)
+    File.join(FIXTURES_DIR, *files)
   end
 
-  def dest_dir(*files)
-    File.join(DEST_DIR, *files)
+  def find_by_title(notes, title)
+    notes.find { |n| n.data["title"] == title }
   end
 
-  # CONFIG_DEFAULTS = {
-  #   "source"      => source_dir,
-  #   "destination" => dest_dir,
-  #   "gems"        => ["jekyll-maps"]
-  # }.freeze
+  # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  
+  # Limits the available syntax to the non-monkey patched syntax that is recommended.
+  # For more details, see:
+  #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
+  #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+  #   - http://myronmars.to/n/dev-blog/2014/05/notable-changes-in-rspec-3#new__config_option_to_disable_rspeccore_monkey_patching
+  config.disable_monkey_patching!
 
-  def make_context(registers = {})
-    Liquid::Context.new({}, {}, { :site => site }.merge(registers))
-  end
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
 
-  def make_site(options = {})
-    site_config = Jekyll.configuration(CONFIG_DEFAULTS.merge(options))
-    Jekyll::Site.new(site_config)
-  end
-
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
 end
