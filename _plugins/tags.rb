d3f8f6cf01968c611de962c8bb1cc15a5437f2a7
manunamz <manunamz@pm.me>
2021-07-16 11:21:53 -0400

    ref: simplify 'sem_tag_posts' liquid filter (relies on next commit to work); and cleanup.

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -1,6 +1,9 @@
 # liquid template to filter between semantic and status tags
 module Jekyll
   module TagFilters
+
+    # filter tags
+    
     def sem_tags(tags)
       return if tags.nil?
       site = @context.registers[:site]
@@ -19,20 +22,6 @@ module Jekyll
       return sem_tags
     end
 
-    def sem_tagged_posts(namespace)
-      return if namespace.nil?
-      site = @context.registers[:site]
-      sem_tagged_posts = []
-      site.collections['entries'].docs.each do |n|
-        site.posts.docs.each do |post|
-          if namespace == n['namespace'] && post['tags'].include?(n['namespace'])
-            sem_tagged_posts << post
-          end
-        end
-      end
-      return sem_tagged_posts
-    end
-
     def stat_tags(tags)
       return if tags.nil?
       site = @context.registers[:site]
@@ -40,11 +29,22 @@ module Jekyll
       site.collections['states'].docs.each do |st|
         tags.each do |t|
           if st['emoji'] == t
-            stat_tags << { 'emoji' => st['emoji'], 'url' => st.url }
+            stat_tags << {
+              'emoji' => st['emoji'],
+              'url' => st.url
+            }
           end
         end
       end
       return stat_tags
+    end
+
+    # filter posts
+
+    def sem_tag_posts(namespace)
+      return if namespace.nil?
+      site = @context.registers[:site]
+      return site.posts.docs.select { |p| p['tags'].include?(namespace) }
     end
   end
 end
