8bcaaa37291d2a46c4fa601bb68376c0c2ed5f20
manunamz <manunamz@pm.me>
2021-09-28 12:00:46 -0400

    ref_scss: cleanup scripts.js.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,9 +8,16 @@ import SiteNav from './site-nav.js';
 // go
 // from: https://stackoverflow.com/questions/9899372/pure-javascript-equivalent-of-jquerys-ready-how-to-call-a-function-when-t
 (() => {
-  // your page initialization code here
-  // the DOM will be available here
-  initListeners();
+  initLinks();
+  
+  document.getElementById('bonsai-burger-nav-checkbox')
+    .addEventListener('click', function(event) {
+      expandSiteNav();
+      document.getElementById('jekyll-graph').dispatchEvent(new Event('draw')); // tell graph to redraw itself
+    }, false);
+
+  applyPencilFilter();
+
   new ThemeColors();
   new SiteNav();
   new GraphNav();
@@ -19,10 +26,9 @@ import SiteNav from './site-nav.js';
   }
 })();
 
-// init
-function initListeners () {
-  // link handling //
+// helpers
 
+function initLinks() {
   // web
   // open external links in new window; wiki-links in current window.
   document.querySelectorAll('a.web-link').forEach(setupLinkOpen);
@@ -31,30 +37,7 @@ function initListeners () {
   document.querySelectorAll('{{ include.wrapperQuerySelector }} a.wiki-link').forEach(setupListeners);
   //  add microdata to wiki links
   document.querySelectorAll('a.wiki-link').forEach((wikiLink) => wikiLink.classList.add('u-url'));
-
-  document.getElementById('bonsai-burger-nav-checkbox')
-    .addEventListener('click', function(event) {
-      expandSiteNav();
-      document.getElementById('jekyll-graph').dispatchEvent(new Event('draw')); // tell graph to redraw itself
-    }, false);
-
-  // apply pencil-filter to all svg images
-  let imgElements = document.getElementsByClassName('embed-image-wrapper');
-  Array.prototype.forEach.call(imgElements, (img) => {
-    // Safari 3.0+ "[object HTMLElementConstructor]" 
-    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
-    if (img.firstElementChild.tagName === "svg" && !isSafari) {
-      if ('{{ site.bonsai.svg.filter }}') {
-        // attach filter to svg element's parent because of safari/mobile bug
-        // bug from: https://github.com/Fyrd/caniuse/issues/3803
-        // workaround from: https://newbedev.com/why-is-filter-drop-shadow-causing-my-svg-to-disappear-in-safari
-        img.style.filter = "url(#PencilTexture)";
-      }
-    }
-  });
 }
-
-// helpers
 
 function setupLinkOpen (link) {
   link.setAttribute("target", "_blank");
@@ -75,4 +58,21 @@ function expandSiteNav() {
     mainView.classList.remove('hide');
     bonsai.hidden = true;
   }
+}
+
+function applyPencilFilter() {
+  // apply pencil-filter to all svg images
+  let imgElements = document.getElementsByClassName('embed-image-wrapper');
+  Array.prototype.forEach.call(imgElements, (img) => {
+    // Safari 3.0+ "[object HTMLElementConstructor]" 
+    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+    if (img.firstElementChild.tagName === "svg" && !isSafari) {
+      if ('{{ site.bonsai.svg.filter }}') {
+        // attach filter to svg element's parent because of safari/mobile bug
+        // bug from: https://github.com/Fyrd/caniuse/issues/3803
+        // workaround from: https://newbedev.com/why-is-filter-drop-shadow-causing-my-svg-to-disappear-in-safari
+        img.style.filter = "url(#PencilTexture)";
+      }
+    }
+  });
 }
